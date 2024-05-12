7e0191db3e4570907892d4ecc37664a6b21b9759
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-12 20:46:31 +0200

    Přidání podmínek

--- src/components/Ukol2/index.jsx
+++ src/components/Ukol2/index.jsx
@@ -1,6 +1,9 @@
 /*
 Zadání: Vytvořte v této komponentě efekt, který se spustí při **prvním** zobrazení komponenty
   a v konzoli vypíše text „Jsem tady“.
+
+  Poznámka: nemáte jak ověřit změnu oproti předchozímu příkladu. Důležité je ale vzykoušet si napsat,
+  jak se píše useEffect, který se zavolá při prvním vykreslení komponenty.
 */
 
 import { useEffect } from "react";
